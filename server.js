4e9432a6b566f963ca45c296652b748201fd3d7c
Harsh Rathod <hrathore50@ymail.com>
2022-05-26 15:20:30 +0530

    Minor fixes 2.

--- server.js
+++ server.js
@@ -18,28 +18,31 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
 // Instruct express to also serve built React client application:
-app.use(express.static("client/build"));
+app.use(express.static("./client/build"));
 
 // Routes:
 app.use("/posts", postRoutes);
 
+// Configuration:
 app.set("port", process.env.PORT || 5000);
 
 const CONNECTION_URL = process.env.DB || "mongodb://localhost:27017/memories";
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(
   CONNECTION_URL,
   { useNewUrlParser: true, useUnifiedTopology: true }
 ).then(() => {
-  app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`Server Running on Port: http://localhost:${PORT}`)
+  });
 }).catch((error) => {
   console.log(`${error} did not connect`);
 });
 
 mongoose.set("useFindAndModify", false);
 
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
