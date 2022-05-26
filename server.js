9080d5c8971af106346167859b1d5810d7bef8b2
Harsh Rathod <hrathore50@ymail.com>
2022-05-26 11:13:01 +0530

    Code refactor and minor fixes.

---
+++ server.js
@@ -0,0 +1,42 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+import postRoutes from "./routes/posts.js";
+
+// Load all environment variables:
+dotenv.config();
+
+// Instantiate the application:
+const app = express();
+
+// Setup the middlewares:
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors());
+
+// Instruct express to also serve built React client application:
+app.use(express.static("client/build"));
+
+// Routes:
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL = process.env.DB || "mongodb://localhost:27017/memories";
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(
+  CONNECTION_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+).then(() => {
+  app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`));
+}).catch((error) => {
+  console.log(`${error} did not connect`);
+});
+
+mongoose.set("useFindAndModify", false);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
