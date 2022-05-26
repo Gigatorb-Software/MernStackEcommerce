1575b1c21e8ee522d6cd4875b1f5dc619892749c
Harsh Rathod <hrathore50@ymail.com>
2022-05-26 16:55:56 +0530

    Axios fix.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://sample-mern-stack.azurewebsites.net/posts';
+const url = 'https://sample-mern-stack.azurewebsites.net/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
